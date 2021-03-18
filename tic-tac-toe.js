16d37595c6a20e251558c3347f934fc6e0046726
Ben <ben_g_smith@msn.com>
2021-03-18 17:56:18 +0100

    added turn and basic level difficulty

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,5 +1,5 @@
 const gameBoard = (() => {
-    const board = Array(9).fill(0);
+    let board = Array(9).fill(0);
 
     const drawTextBoard = () => {
         for (let i = 0; i < board.length; i++) {
@@ -10,34 +10,48 @@ const gameBoard = (() => {
         console.log("-------------");
     };
 
-    const updateBoard = (boardDiv) => {
-        board.forEach((cell, i) => { boardDiv.children[i].className = " XO"[cell] });
-    }
-
     const setCell = (cell, value) => {
         board[cell-1] = value; 
+    }
+
+    const getValidMoves = () => {
+        // !board[i] returns all non-falsy items. Non-falsy includes non-zero items 
+        // so it returns free cells as free cells are zeros.  
+        return [...board.keys()].filter(i => !board[i])
     }
 
     const getCell = (cell) => board[cell-1];
     const getLength = () => board.length; 
     const getBoard = () => board;
+    const resetBoard = () => board = Array(9).fill(0);
 
-    return { drawTextBoard, setCell, getCell, getLength, getBoard, updateBoard }
+    return { drawTextBoard, setCell, getCell, getLength, getBoard, getValidMoves,
+            resetBoard }
 })();
 
 const game = (() => {
-    const moves = [];
+    let moves = [];
     let paused = false;
 
     const setPaused = (state) => paused = state;
-    const getPaused = () => paused; 
+    const isPaused = () => paused; 
 
     // Regex for isWin condition, see https://stackoverflow.com/a/65979060/15011558
     const isWin = () => /^(?:...)*([12])\1\1|^.?.?([12])..\2..\2|^([12])...\3...\3|^..([12]).\4.\4/.test(gameBoard.getBoard().join(""));
     const isDraw = () => moves.length === gameBoard.getLength();
-    const isPlayable = () => !isWin() && !isDraw();
-
+    const isPlayable = () => !isWin() && !isDraw() && !isPaused();
     const turn = () => 1 + moves.length % 2;
+    const getMoves = () => moves.length;
+    const getgameStatus = (player) => {
+        if (isWin()) {
+            let winner = turn() % 2 + 1;
+            return `${player.getName()} wins!`;
+        }
+        if (isDraw()) return `It's a draw`;
+        if (isPlayable()) return ``;
+        if (isPaused()) return ``;
+    }
+
     const play = (move) => {
         if(gameBoard.getCell(move) !== 0 || !game.isPlayable()) return false; 
         gameBoard.setCell(move, turn());
@@ -47,9 +61,16 @@ const game = (() => {
         return true; 
     }
 
-    const getMoves = () => moves.length
+    const resetGame = () => {
+        gameBoard.resetBoard();
+        moves = [];
+        paused = false;
+        DOM.resetBoard();
+        DOM.updategameStatus();
+    }
     
-    return { play, getMoves, turn, isWin, isDraw, isPlayable, setPaused, getPaused }
+    return { play, getMoves, turn, isWin, isDraw, isPlayable, setPaused, 
+            isPaused, getgameStatus, resetGame }
 })();
 
 const Player = (name, playerType) => {
@@ -63,49 +84,96 @@ const Player = (name, playerType) => {
 
     function computerMove() {
         if (!game.isPlayable()) return false;
-        move = Math.floor(Math.random() * 9) + 1
+        moves = gameBoard.getValidMoves();
+        move = moves[Math.floor(Math.random() * moves.length)] + 1; // random
+        console.log("Computer playing " + move);
         return game.play(move);
     }
 
     const isHuman = () => (playerType == "Human") ? true : false;
 
-    return { humanMove, computerMove, isHuman }
+    return { getName, humanMove, computerMove, isHuman }
 }
 
-(function main() {
+const DOM = (() => {
     const boardDiv = document.querySelector("#game-container");
-    const playerOne = Player('Ben', "Human");
-    const playerTwo = Player('Robot', "Bot");
+    const message = document.querySelector("#message");
+    const gameDiv = document.querySelector("#game-container");
+    const resetButton = document.querySelector("#reset-button")
 
-    boardDiv.addEventListener("click", (e) => {
+    const getBoardDiv = () => boardDiv;
+    const getMessage = () => message;
+    const getResetButton = () => resetButton;
+    const updategameStatus = (player) => {
+        message.innerHTML = game.getgameStatus(player);
+        if (!game.isPlayable()) { 
+            gameDiv.classList.add('inactive');
+        } else {
+            gameDiv.classList.remove('inactive');
+        }
+    }
+
+    const updateBoard = () => {
+        board = gameBoard.getBoard();
+        board.forEach((cell, i) => { 
+            boardDiv.children[i].className = " XO"[cell] 
+        });
+    }
+
+    const resetBoard = () => {
+        board = gameBoard.getBoard();
+        board.forEach((cell, i) => { 
+            boardDiv.children[i].className = "";
+        });
+    }
+
+    return { getBoardDiv, getMessage, updategameStatus, updateBoard, resetBoard,
+             getResetButton }
+
+})();
+
+(function main() {
+    const playerOne = Player('Carla', "Human");
+    const playerTwo = Player('Ben-Bot', "Bot");
+
+    DOM.getResetButton().addEventListener("click", (e) =>{
+        game.resetGame();
+    });
+
+    DOM.getBoardDiv().addEventListener("click", (e) => {
         let move = Array.prototype.indexOf.call(e.target.parentNode.children, e.target) + 1;
-        let round = 0;
 
-        while (round < 2 && game.isPlayable()) {
-            while(game.getPaused() == false) {
-                if (!(game.turn() % 2) == 0) {
-                    console.log("Player 1");
-                    if (playValidMove(playerOne, move)) round++;
-                    gameBoard.updateBoard(boardDiv);
-                } else {
-                    console.log("Player 2");
-                    if (playValidMove(playerTwo, move)) round++;
-
-                    game.setPaused(true);
-                    setTimeout(() => {
-                        game.setPaused(false);
-                        gameBoard.updateBoard(boardDiv);
-                    }, 500); 
-                }
+        if (game.isPaused() == false && game.isPlayable()) {
+            if (game.turn() == 1) {
+                console.log("Player 1");
+                playValidMove(playerOne, move);
+                DOM.updateBoard();
+                DOM.updategameStatus(playerOne);
+            } 
+            
+            if (!playerTwo.isHuman() && game.isPlayable() && game.turn() ==2) {
+                console.log("Player 2");
+                console.log(gameBoard.getValidMoves());
+                playValidMove(playerTwo, move);
             }
         }
+
     });
 
     function playValidMove(player, move) {
         if (player.isHuman()) {
-            return player.humanMove(move);
+            player.humanMove(move);
         } else { 
-            return player.computerMove();
+            game.setPaused(true);
+            DOM.updategameStatus();
+            console.log("paused");
+            setTimeout(() => {
+                console.log("unpaused");
+                game.setPaused(false);
+                player.computerMove();
+                DOM.updateBoard();
+                DOM.updategameStatus(playerTwo);
+            }, 500); 
         };
     }
 
